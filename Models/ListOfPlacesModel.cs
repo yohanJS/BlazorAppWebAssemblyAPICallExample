eb9d0a9d24f2c16ecdb4c606ead63e801a17525a
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2023-09-10 10:24:11 -0400

    Add project files.

---
+++ Models/ListOfPlacesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorAppWebAssembly.Models
+{
+    public class ListOfPlacesModel
+    {
+        [JsonPropertyName("place name")]
+        public string? place_name { get; set; }
+        public string? longitude { get; set; }
+        public string? state { get; set; }
+        [JsonPropertyName("state abbreviation")]
+        public string? state_abbreviation { get; set; }
+        public string? latitude { get; set; }
+    }
+}
---
+++ Models/ZipCodePrimaryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorAppWebAssembly.Models
+{
+    public class ZipCodePrimaryModel
+    {
+        [JsonPropertyName("post code")]
+        public string? post_code { get; set; }
+        public string? country { get; set; }
+        [JsonPropertyName("country abrebiation")]
+        public string? country_abrebiation { get; set; }
+
+        public List<ListOfPlacesModel>? places { get; set; }
+    }
+}
+
